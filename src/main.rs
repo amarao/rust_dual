35df35bac31644fbdad5d7cf10e42d8cd48b0d0c
George Shuklin <george.shuklin@gmail.com>
2021-03-15 21:31:43 +0200

    Made it work?

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,88 @@
 use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering::Relaxed;
 
-struct Multiwrite{
+struct Multiwrite {
     vec: Vec<AtomicU32>,
-    slice1: &'static [AtomicU32],
-    slice2: &'static mut [AtomicU32],
-    clone: bool,
-    thread: Option<std::thread::JoinHandle<()>>
-    
+    slice: &'static mut [AtomicU32],
+    thread: Option<std::thread::JoinHandle<()>>,
 }
 
-impl Multiwrite{
-    fn new(size:usize, init_value: u32) -> Self{
+impl Multiwrite {
+    fn new(size: usize, init_value: u32) -> Self {
         let mut vec = Vec::with_capacity(size);
-        vec.resize_with(size, ||AtomicU32::new(init_value));
+        vec.resize_with(size, || AtomicU32::new(init_value));
         let ptr = vec.as_mut_ptr();
-        unsafe{
-            Multiwrite{
+        unsafe {
+            Multiwrite {
                 vec,
-                slice1: std::slice::from_raw_parts(ptr, size),
-                slice2: std::slice::from_raw_parts_mut(ptr, size),
-                clone: false,
-                thread: None
+                slice: std::slice::from_raw_parts_mut(ptr, size),
+                thread: None,
             }
         }
     }
 
-    fn get(&self, idx: usize) -> u32{
-        self.slice1[idx].load(Relaxed)
+    fn get(&self, idx: usize) -> u32 {
+        self.slice[idx].load(Relaxed)
     }
-    fn set(&mut self, idx: usize, val: u32){
-        self.slice2[idx].store(val, Relaxed);
+    fn set(&mut self, idx: usize, val: u32) {
+        self.slice[idx].store(val, Relaxed);
     }
 
-    fn spawn(& mut self){
-        let size = self.slice1.len();
-        unsafe{
+    fn spawn<F>(&mut self, f: F)
+    where
+        F: FnOnce(Multiwrite) + Clone + Send + 'static,
+    {
+        let size = self.slice.len();
+        unsafe {
             let ptr = self.vec.as_mut_ptr();
-                
-            let mut cloned = Multiwrite{
+
+            let mut cloned = Multiwrite {
                 vec: Vec::new(),
-                slice1: std::slice::from_raw_parts(ptr, size),
-                slice2: std::slice::from_raw_parts_mut(ptr, size),
-                clone: true,
-                thread: None
+                slice: std::slice::from_raw_parts_mut(ptr, size),
+                thread: None,
             };
-            self.thread = Some(std::thread::spawn(move ||{
-                let mut cnt: u32 = 0;
-                loop{
-                    cloned.set(0, cnt);
-                    cnt +=1;
-                }
-            }))
+            self.thread = Some(std::thread::spawn(move || f(cloned)));
         }
-        
     }
 }
 
-
-
-impl Drop for Multiwrite{
-    fn drop(&mut self){
-        if self.clone{
-            loop{}
+impl Drop for Multiwrite {
+    fn drop(&mut self) {
+        if self.thread.is_some() {
+            println!("Other thread is found running, terminating.");
+            std::process::exit(0);
         }
-        if let Some(thread) = &self.thread {
-            println!("Other thread found, terminating.");
-            // std::process::exit(0);
-        }
+    }
+}
+
+fn loop_set(mut mw: Multiwrite) {
+    let mut cnt: u32 = 0;
+    loop {
+        mw.set(0, cnt);
+        cnt += 1;
     }
 }
 
 fn main() {
     println!("Hello, world!");
     let mut a = Multiwrite::new(1, 0);
-    a.spawn();
-    loop{
+    a.spawn(loop_set);
+    loop {
         println!("{}", a.get(0));
+        if a.get(0) >= 65536 {
+            break;
+        }
     }
 }
 
 #[test]
-fn test_create(){
-    let a = Multiwrite::new(1,0);
+fn test_create() {
+    let a = Multiwrite::new(1, 0);
     assert_eq!(a.get(0), 0);
 }
 #[test]
-fn test_write(){
-    let mut a = Multiwrite::new(1,0);
+fn test_write() {
+    let mut a = Multiwrite::new(1, 0);
     a.set(0, 42);
     assert_eq!(a.get(0), 42);
 }
