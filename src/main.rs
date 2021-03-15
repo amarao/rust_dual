c16263f5a8b62f8a5ec997f6ec0eb2e7c9191f41
George Shuklin <george.shuklin@gmail.com>
2021-03-15 21:24:49 +0200

    Make it work?

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering::Relaxed;
 
 struct Multiwrite{
+    vec: Vec<AtomicU32>,
     slice1: &'static [AtomicU32],
     slice2: &'static mut [AtomicU32],
     clone: bool,
@@ -13,13 +14,12 @@ impl Multiwrite{
     fn new(size:usize, init_value: u32) -> Self{
         let mut vec = Vec::with_capacity(size);
         vec.resize_with(size, ||AtomicU32::new(init_value));
-        let slice = vec.as_mut_slice();
-        let slice1 = slice.as_ptr();
-        let slice2 = slice.as_mut_ptr();
+        let ptr = vec.as_mut_ptr();
         unsafe{
             Multiwrite{
-                slice1: std::slice::from_raw_parts(slice1, size),
-                slice2: std::slice::from_raw_parts_mut(slice2, size),
+                vec,
+                slice1: std::slice::from_raw_parts(ptr, size),
+                slice2: std::slice::from_raw_parts_mut(ptr, size),
                 clone: false,
                 thread: None
             }
@@ -36,17 +36,24 @@ impl Multiwrite{
     fn spawn(& mut self){
         let size = self.slice1.len();
         unsafe{
-            let ptr1 = self.slice1.as_ptr();
-            let ptr2_non_mut = self.slice1.as_ptr();
-            let ptr2 = ptr2_non_mut as *mut AtomicU32;
-            let clone = Multiwrite{
-                slice1: std::slice::from_raw_parts(self.slice2.as_ptr(), size),
-                slice2: std::slice::from_raw_parts_mut(self.slice2.as_mut_ptr(), size),
+            let ptr = self.vec.as_mut_ptr();
+                
+            let mut cloned = Multiwrite{
+                vec: Vec::new(),
+                slice1: std::slice::from_raw_parts(ptr, size),
+                slice2: std::slice::from_raw_parts_mut(ptr, size),
                 clone: true,
                 thread: None
             };
+            self.thread = Some(std::thread::spawn(move ||{
+                let mut cnt: u32 = 0;
+                loop{
+                    cloned.set(0, cnt);
+                    cnt +=1;
+                }
+            }))
         }
-        self.thread = Some(std::thread::spawn(||{loop{}}))
+        
     }
 }
 
@@ -59,19 +66,18 @@ impl Drop for Multiwrite{
         }
         if let Some(thread) = &self.thread {
             println!("Other thread found, terminating.");
-            std::process::exit(0);
-        }
-        unsafe{
-            
-            self.slice2  = std::slice::from_raw_parts_mut(self.slice2.as_mut_ptr(), 0)
-                
+            // std::process::exit(0);
         }
     }
 }
 
 fn main() {
     println!("Hello, world!");
-    // let thread = std::thread::spawn(||)
+    let mut a = Multiwrite::new(1, 0);
+    a.spawn();
+    loop{
+        println!("{}", a.get(0));
+    }
 }
 
 #[test]
@@ -83,5 +89,5 @@ fn test_create(){
 fn test_write(){
     let mut a = Multiwrite::new(1,0);
     a.set(0, 42);
-    // assert_eq!(a.get(0), 42);
+    assert_eq!(a.get(0), 42);
 }
